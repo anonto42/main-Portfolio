ce3bfd686fb119fe98e5839960db3123bbb1b9fa
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-06-03 15:15:18 +0600

    footer added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { FaFacebookSquare, FaGithub, FaLinkedin } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='w-ful h-[100px] text-center bg-[#1c222a00] md:flex justify-around items-center'>
+      <div className='flex justify-center pt-5'>
+        <div className='round'>
+          <a target='blnk' href="https://www.linkedin.com/in/sohidul-islam-ananto/">
+            <FaLinkedin />
+          </a>
+        </div>
+        <div className='round mx-8'>
+          <a target='blnk' href="https://github.com/anonto42">
+            <FaGithub />
+          </a>
+        </div>
+        <div className='round'>
+          <a target='blnk' href="https://www.facebook.com/badboy.m10.33/">
+            <FaFacebookSquare />
+          </a>
+        </div>
+      </div>
+      <div className='pt-3'>
+        <h5 className='text-[#80b4f4]'>© All Rights Reserved 2015 - 2023 | Sohidul Islam Ananto</h5>
+      </div>
+    </div>
   )
 }
 
