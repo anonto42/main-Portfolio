c235b73a32f94a3d817d669792cb52fe9435e2f8
Sohidul Islam Ananto <anontom90@gmail.com>
2024-08-23 13:29:03 +0100

    Data Updated

--- src/Components/Messages/Message.jsx
+++ src/Components/Messages/Message.jsx
@@ -3,14 +3,14 @@ import { ImBin } from "react-icons/im";
 
 const Message = ( { email , name , work } ) => {
   return (
-    <div className='w-full rounded-md min-h-[60px] bg-[#4285F4] mt-5'>
+    <div className='w-full rounded-md min-h-[60px] bg-[#254c8962] mt-5'>
         {/* Information */}
         <div className='flex italic text-sm justify-around px-3 py-3 w-full text-white'>
             <div className=' w-[400px] text-center'>
-                <h3>{name}</h3>
+                <h3>name : {name}</h3>
             </div>
             <div className='w-[400px] text-center'>
-                <h3>{email}</h3>
+                <h3>email : {email}</h3>
             </div>
         </div>
         {/* work message */}
