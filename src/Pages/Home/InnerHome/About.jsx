7cef2af8a3c612b8c51e9c4ca229efe32e8d7204
Sohidul Islam Ananto <anontom90@gmail.com>
2024-08-24 04:52:46 +0100

    Data updated

--- src/Pages/Home/InnerHome/About.jsx
+++ src/Pages/Home/InnerHome/About.jsx
@@ -5,7 +5,7 @@ import { BsEmojiSunglassesFill } from 'react-icons/bs'
 import axios from 'axios'
 
 const About = () => {
-  
+
   useGSAP(()=>{
     gsap.from('.gsap_animation',{
       y:20,
@@ -35,7 +35,7 @@ const About = () => {
           <h3 className='text-[25px] gsap_animation font-semibold'>Hello, My Name Is</h3>
           <h1 className='text-[30px] gsap_animation font-bold'>Sohidul Islam Ananto</h1>
           <h2 className='text-[27px] gsap_animation gsap_animation font-bold text-[#80b4f4]'>I AM Web Developer</h2>
-          <p className='gsap_animation'>From Dhaka, Bangladesh. I’m a (MERN) stack Web developer who is passionate about making error-free websites. I love to write code's. </p>
+          <p className='gsap_animation'>I’m a ( MERN ) stack Web developer. I make error-free and user friendly websites that will give your user a good experience. I love to write code's. </p>
           <a href="/contact-me">
             <button id='' className='animation_up_down1 hover:bg-[#232323] bg-[#80b4f4] duration-300 ease-out hover:px-3 hover:text-white font-semibold text-[#1A1A33] px-1 py-2 w-[95px] rounded-xl text-[16px] shadow-md shadow-[#ffffff7b] my-[35px]'>Contact</button>
           </a>
@@ -48,12 +48,12 @@ const About = () => {
             </div>
           </div>
           <div id='' className='animation_up_down animation_up_down01 flex justify-center items-center w-[95%] h-[80px] border-[#80b4f4] border-[3px] rounded-[30px] md:absolute md:top-[450px] md:w-[100px] md:block md:-ml-[40px] md:px-8 md:bg-[#18182E] md:-mt-12 relative'>
-            <h1 className='flex justify-center font-bold md:text-[17px] mt-[5px]'>{'1.5'}+</h1>
+            <h1 className='flex justify-center font-bold md:text-[17px] mt-[10px]'>{'1.6'}+</h1>
             <p className='font-semibold italic md:text-[10px] md:absolute left-[21px]'>Years of Experience</p>
           </div>
           <div id='' className='animation_up_down animation_up_down2 flex justify-center items-center w-[95%] mt-3 h-[80px] border-[#80b4f4] border-[3px] rounded-[30px] md:absolute md:top-[450px] md:w-[100px] md:block md:ml-[105px] md:px-8 md:bg-[#18182E] md:mt-16 relative'>
-            <h1 className='flex justify-center font-bold md:text-[17px] mt-[5px]'>{projectsLenth}+</h1>
-            <p className='font-semibold italic md:text-[10px] md:absolute left-[21px]'>Completed Projects</p>
+            <h1 className='flex justify-center font-bold md:text-[17px] mt-[10px]'>{projectsLenth}+</h1>
+            <p className='font-semibold italic md:text-[10px] md:absolute left-[21px] bottom-3'>Completed Projects</p>
           </div>
         </div>
       </div>
