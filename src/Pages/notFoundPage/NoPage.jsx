e11e51a0654296a12ed26dc9b303755eda14218d
Sohidul Islam Ananto <anontom90@gmail.com>
2024-08-28 22:03:59 +0600

    wrong page added

---
+++ src/Pages/notFoundPage/NoPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NoPage = () => {
+  return (
+    <div className='bg-[black] h-svh w-full flex justify-center items-center text-white'>
+      <div className='text-3xl font-bold'>
+        You ar comed in a wrong place
+      </div>
+    </div>
+  )
+}
+
+export default NoPage
