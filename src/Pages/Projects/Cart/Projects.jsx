5d8824d4b18feab140039ed4cc05287556ba620f
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-11-06 17:19:37 +0600

    project cart design updated

--- src/Pages/Projects/Cart/Projects.jsx
+++ src/Pages/Projects/Cart/Projects.jsx
@@ -10,20 +10,20 @@ const Projects = ({imgUrl,liveLink,repoLink,title}) => {
 
     })
     return (
-        <div className='bg-[#1C222A] mb-[50px] w-full md:w-[380px] rounded-2xl shadow-xl pb-5 px-5 mr-6'>
+        <div className='border-[#1C222A] border-[3px] mb-[50px] w-full md:w-[380px] rounded-2xl shadow-xl pb-5 px-5 mr-6'>
             <div className='w-[full] md:h-[150px] scale-[1.12] rounded-lg h-[200px] mb-3 outline-none overflow-hidden'>
                 <img className='w-full h-full rounded-lg' src={imgUrl} />
             </div>
-            <div className='w-full h-[30px] text-center text-white capitalize italic'>
-                <h3 className='font-thin'>{title}</h3>
+            <div className='w-full h-[30px] text-center text-white my-3 pt-2 capitalize italic'>
+                <h3 className=' font-normal'>{title}</h3>
             </div>
             <div className='flex justify-around'>
-                <a href={liveLink} target='blank'>
+                <a href={liveLink} target='_blank'>
                     <div className='w-[100px] md:w-[80px] md:h-[30px] md:text-[#515151] h-[40px] bg-[#88B8F4] rounded-lg flex justify-center items-center md:text-[14px] shadow-md hover:shadow-[#5b79bf] shadow-[#312f453f] active:scale-110 cursor-pointer duration-150 ease-linear font-semibold'>
                         Live link
                     </div>
                 </a>
-                <a href={repoLink}>
+                <a href={repoLink} target='_blank'>
                     <div className='w-[100px] md:text-[14px] md:w-[80px] md:h-[30px] md:text-[#515151] h-[40px] bg-[#88B8F4] rounded-lg flex justify-center items-center shadow-md hover:shadow-[#5b79bf] shadow-[#312f453f] active:scale-110 cursor-pointer duration-150 ease-linear font-semibold'>
                         Code link
                     </div> 
