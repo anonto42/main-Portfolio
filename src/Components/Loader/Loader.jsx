66c72ab88b7b082321c709106916848bc7ad1168
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-12-21 11:41:33 +0600

    Hight related context updated

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import SpLoader from './SpLoader'
 const Loader = () => {
   return (
     <div
-        className='bg-transparent h-full w-full flex justify-center items-center text-center'
+        className='bg-transparent min-h-[100svh] w-full flex justify-center items-center text-center'
     >
         <SpLoader/>
     </div>
