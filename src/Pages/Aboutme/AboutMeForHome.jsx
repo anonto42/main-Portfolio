a3683aa15c996197be6171fa32f8003421a624da
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-12-21 11:23:43 +0600

    This page is for home Page information

---
+++ src/Pages/Aboutme/AboutMeForHome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from './../Layout/Layout';
+import Contact from './../Home/InnerHome/Contact';
+import Introduction from './innerThisPage/Introduction';
+
+const AboutMeForHome = () => {
+  return (
+    <div>
+      <Introduction/>
+    </div>
+  )
+}
+
+export default AboutMeForHome
