00e134ff5d33e895379dff4c04fcd4bf631dcf73
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-12-21 14:55:20 +0600

    Text input animation

--- src/Pages/Aboutme/innerThisPage/Introduction.jsx
+++ src/Pages/Aboutme/innerThisPage/Introduction.jsx
@@ -1,10 +1,26 @@
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from 'gsap'
 import React from 'react';
 
 const Introduction = () => {
+    gsap.registerPlugin(ScrollTrigger)
+    useGSAP(()=>{
+        gsap.from('.text-animation-in',{
+            y:20,
+            opacity:0,
+            duration:2,
+            scrollTrigger:{
+                trigger:'.box-of-animation',
+                start:"top 70%",
+                scroller:"body"
+            }
+        });
+    })
     return (
         <div className='w-full h-auto text-white py-10'>
-            <h1 className='text-[25px] text-center underline text-[#80b4f4] font-bold md:text-[30px] my-5'>ABOUT ME</h1>
-            <p className='text-[15px] font-light leading-8'>Hey there, welcome to my corner of the web! I'm Sohidul Islam Ananto, a web developer based in Dhaka, Bangladesh. As a Computer Science and Engineering student, I'm deeply passionate about crafting error-free websites that leave a lasting impression. With my expertise in the<span className='font-bold text-[#ffffff]  rounded-xl bg-[#80b4f4] mx-2'> MERN stack </span>, I thrive on writing clean, efficient code that brings ideas to life. Take a peek into my portfolio and discover the seamless blend of creativity and precision in every project.</p>
+            <h1 className='text-animation-in text-[25px] text-center underline text-[#80b4f4] font-bold md:text-[30px] my-5'>ABOUT ME</h1>
+            <p className='text-[15px] font-light leading-8 text-animation-in'>Hey there, welcome to my corner of the web! I'm Sohidul Islam Ananto, a web developer based in Dhaka, Bangladesh. As a Computer Science and Engineering student, I'm deeply passionate about crafting error-free websites that leave a lasting impression. With my expertise in the<span className='font-bold text-[#ffffff]  rounded-xl bg-[#80b4f4] mx-2'> MERN stack </span>, I thrive on writing clean, efficient code that brings ideas to life. Take a peek into my portfolio and discover the seamless blend of creativity and precision in every project.</p>
         </div>
     );
 }
