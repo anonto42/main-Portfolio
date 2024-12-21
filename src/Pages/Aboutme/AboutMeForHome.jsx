0aa043b5b38e68730575285b27e1aac6f38758f6
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-12-21 14:50:34 +0600

    extra code cleaning

--- src/Pages/Aboutme/AboutMeForHome.jsx
+++ src/Pages/Aboutme/AboutMeForHome.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Layout from './../Layout/Layout';
-import Contact from './../Home/InnerHome/Contact';
 import Introduction from './innerThisPage/Introduction';
 
 const AboutMeForHome = () => {
