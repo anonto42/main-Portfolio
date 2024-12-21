4167ba3853ff4e6752a6dcf310680cf6d317c83c
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-12-21 11:24:15 +0600

    added react lazy on all components and the pages infor was added on the home pages

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import Layout from '../Layout/Layout'
-import About from './InnerHome/About'
+import React, { Suspense, useEffect, useState } from 'react'
 import MyServices from './InnerHome/MyServices'
-import Contact from './InnerHome/Contact'
+import axios from 'axios'
+import Loader from '../../Components/Loader/Loader'
+const AboutMeForHome = React.lazy(()=> import( '../Aboutme/AboutMeForHome'))
+const Contact = React.lazy(()=> import('./InnerHome/Contact' ))
+const About = React.lazy(()=> import('./InnerHome/About' ))
+const Layout = React.lazy(()=> import('../Layout/Layout' ))
+const ProjectForHomePage = React.lazy(()=> import('./../Projects/ProjectForTheHomePage' ))
 
 const Home = () => {
+
+  const [projectsLenth , setProjects] = useState([]);
+
+  useEffect(()=>{
+
+    axios.get('https://main-porthfolio-backend.vercel.app/api/admin/getProjecs')
+    .then( e => setProjects(e.data.length) )
+    .catch( e => console.log(e) )
+
+  },[]);
+
   return (
     <Layout>
-        <About/>
+      <Suspense fallback={<Loader />} >
+        <About projectsLenth={projectsLenth}/>
         <MyServices/>
+        <ProjectForHomePage />
+        <AboutMeForHome />
         <Contact />
+      </Suspense>
     </Layout>
   )
 }
