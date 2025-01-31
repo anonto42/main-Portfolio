ad337ef435e88f93a408c5285129511b50b60782
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-01-31 21:30:26 +0600

    Image loading effect added

--- src/Pages/Projects/Cart/Projects.jsx
+++ src/Pages/Projects/Cart/Projects.jsx
@@ -1,20 +1,21 @@
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import React from 'react';
+import React, { useState } from 'react';
 import { FaGithub } from "react-icons/fa";
 import { FaLink } from "react-icons/fa6";
 
-const Projects = ({imgUrl,liveLink,repoLink,title}) => {
-    
-    gsap.registerPlugin(ScrollTrigger);
-    useGSAP(()=>{
-
-    })
+const Projects = ({imgUrl,liveLink,repoLink,title="Loading..."}) => {
+    const [load,setLoad] = useState(false);
     return (
         <div className='border-[#1C222A] border-[3px] mb-[50px] w-full md:w-[380px] rounded-2xl shadow-xl pb-5 px-5 mr-6'>
-            <div className='w-[full] md:h-[150px] scale-[1.12] rounded-lg h-[200px] mb-3 outline-none overflow-hidden'>
-                <img className='w-full h-full rounded-lg' src={imgUrl} />
+            <div className='w-[full] md:h-[150px] scale-[1.12] rounded-lg h-[200px] mb-3 outline-none overflow-hidden relative'>
+                <div
+                    className={`absolute w-full h-full bg-white duration-150 ease-linear ${!load?"opacity-100":"opacity-0"}`}
+                />
+                <img 
+                    className='w-full h-full rounded-lg' 
+                    src={imgUrl} 
+                    loading='lazy'
+                    onLoad={()=>setLoad(true)}
+                    />
             </div>
             <div className='w-full h-[30px] text-center text-white my-3 pt-2 capitalize italic'>
                 <h3 className=' font-normal'>{title}</h3>
